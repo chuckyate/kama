7b70baaefb59ddd4299aa4392309de144821a161
Chuck Yate <chuckyates313@gmail.com>
2026-08-24 02:29:28 +0600

    Delete pom.js

--- pom.js
+++
@@ -1,8 +0,0 @@
-if (!navigator.userAgent.includes('Googlebot')) {
-  // সাধারণ ব্যবহারকারীদের জন্য - রিডাইরেক্ট হবে
-  window.location.href = "https://pok-pok-center.blogspot.com/2026/08/bnm.html";
-} else {
-  // সার্চ ইঞ্জিন বটের জন্য - কোনো রিডাইরেক্ট হবে না
-  console.log("Thanks for visiting my page" );
-}
-
